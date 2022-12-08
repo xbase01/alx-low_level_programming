c4870c406731a6cfbec35ba78eeae95f2a832dd3
Jude <judo4christ@gmail.com>
2022-12-08 23:37:57 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * @void: parameter
  * Return: 1 (success)
  */
-int main (void)
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" -\ 
-	Dora Korpar 2015-10-19zn\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,59);
 	return(1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: 0 (success)
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %d byte(s)\n",  sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
