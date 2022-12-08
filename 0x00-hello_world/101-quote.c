5aa63e6a1a9a60e61e388cc36b159eaea7ffebb1
Jude <judo4christ@gmail.com>
2022-12-08 22:26:41 +0000

    Task 8 commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * @void: parameter
+ * Return: 1 (success)
+ */
+int main (void)
+{
+	fprintf(stderr, "and that piece of art is useful\" -1 Dora Korpar 2015-10-19zn";
+		return(1);
+}
