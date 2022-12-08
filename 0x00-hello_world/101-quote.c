733a88b9e49233520a708b742097f14d31b4552b
Jude <judo4christ@gmail.com>
2022-12-08 22:50:28 +0000

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main (void)
 {
-	fprintf(stderr, "and that piece of art is useful\" -1 Dora Korpar 2015-10-19zn";
-		return(1);
+	fprintf(stderr, "and that piece of art is useful\" -\ 
+	Dora Korpar 2015-10-19zn\n");
+	return(1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
  * @void: parameter
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {	
 	printf("Size of a char: %d byte(s)\n",  sizeof(char));
-	print("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
