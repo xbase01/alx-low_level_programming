69c805e46d976e3c0ffa517a72e1375e2b1863e1
Jude <judo4christ@gmail.com>
2022-12-09 23:15:01 +0000

    commit lasdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	lasdigit = n % 10;
 	if (lasdigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lasdigit);
 	else if (lasdigit == 0)
