734315d60ff3471ab8083e54b3de03583e6a1335
Jude <judo4christ@gmail.com>
2022-12-10 02:46:03 +0000

    102-print_comb5 corrected

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int num1, num2;
-	
+
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -28,7 +28,7 @@ int main(void)
 			}
 		}
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
