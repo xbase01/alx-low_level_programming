667719de929fd61b1fe7d31914e38e2a277bc6ef
Jude <judo4christ@gmail.com>
2022-12-10 02:42:17 +0000

    task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,20 @@
 */
 int main(void)
 {
-	int digit1, digit2, digit3;
-	
-	for (digit1 = 0; digit1 < 8; digit1++)
+	int d1, d2, d3;
+
+	for (d1 = 0; d1 < 8; d1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (d2 = d1 + 1; d2 < 9; d2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			for (d3 = d2 + 1; d3 < 10; d3++)
 			{
-				if (!((digit3 == digit2) || (digit2 == digit1) || (digit2 > digit3) || (digit1 > digit2)))
+				if (!((d3 == d2) || (d2 == d1) || (d2 > d3) || (d1 > d2)))
 				{
-					putchar((digit1 % 10) + '0');
-					putchar((digit2 % 10) + '0');
-					putchar((digit3 % 10) + '0');
-					if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					putchar((d1 % 10) + '0');
+					putchar((d2 % 10) + '0');
+					putchar((d3 % 10) + '0');
+					if (d1 == 7 && d2 == 8 && d3 == 9)
 						continue;
 					putchar(',');
 					putchar(' ');
