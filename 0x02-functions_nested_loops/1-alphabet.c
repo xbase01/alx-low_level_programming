dc57280a9198e4ab1b0596728fa8cf5ae8b2c9a8
Jude <judo4christ@gmail.com>
2022-12-13 22:55:10 +0000

    Commit 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
