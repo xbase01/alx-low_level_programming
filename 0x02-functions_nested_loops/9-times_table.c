20fdabaa7a75caa533ca9c93b2727f0f598186dd
Jude <judo4christ@gmail.com>
2022-12-14 04:58:44 +0000

    now

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,20 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 int product = i * j;
-
-if (product >= 10)
+if (j > 0)
 {
+_putchar(',');
+_putchar(' ');
+
+if (product < 10)
+{
+_putchar(' ');
+}
+}
+
 _putchar('0' + (product / 10));
 _putchar('0' + (product % 10));
 }
-else
-{
-_putchar('0' + product);
-}
-
-_putchar(',');
-_putchar(' ');
-}
-
 _putchar('\n');
 }
 }
