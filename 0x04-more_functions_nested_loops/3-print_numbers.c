8684adc6b5ab197c121aa88173eda53422789155
Jude <judo4christ@gmail.com>
2022-12-15 21:15:41 +0000

    print numbers commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - it prints numbers 0 to 9
+ *Return: Returns void
+ */
+
+int print_numbers(void)
+{
+int i;
+
+for (i = 0, i <= 9, i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+return (0);
+}
