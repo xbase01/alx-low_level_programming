90e37aa3412617c7f4303d09dcf965a3fd3329b6
Jude <judo4christ@gmail.com>
2022-12-15 12:02:23 +0000

    0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints 0 is zero if integer is 0
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
