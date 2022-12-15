aa00bee934b45955bddef932719823b1a091a026
Jude <judo4christ@gmail.com>
2022-12-15 04:22:05 +0000

    commit 101-natural2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- *main - computes the sum of all the multiples of 3 and 5 below 1024
+ *main - computes the sum of all the multiples of 3 or 5 below 1024
  *
- *@sum: character to execute
+ *@n: n-1 is used as the upper limit
+ *@i: is used to in loop to check multiple of 3 or 5
+ *@sum: store multiple of 3 and 5
  *
  *Return: returns 0
  */
@@ -20,6 +22,6 @@ if (i % 3 == 0 || i % 5 == 0)
 sum = sum + i;
 }
 }
-printf("%d", sum);
+printf("%d \n", sum);
 return (0);
 }
