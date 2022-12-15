1679882f0947d6e5daccbd18e140929ca0931a4c
Jude <judo4christ@gmail.com>
2022-12-15 20:46:10 +0000

    first commit to 0x04-more... directory

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper - checks if input is uppercase
+ *@c: character to check
+ *Return: Returns 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit
+ *@c: character to check
+ *Return: Returns 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
