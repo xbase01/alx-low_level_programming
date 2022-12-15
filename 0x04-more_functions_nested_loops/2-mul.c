8e9e15269a2626337c61690836c023fc49fb3c16
Jude <judo4christ@gmail.com>
2022-12-15 21:05:11 +0000

    commit multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: character to multiplier
+ *@b: character to multiplier
+ *Return: Returns c
+ */
+int mul(int a, int b)
+{
+int c;
+
+c = (a * b);
+return (c);
+}
