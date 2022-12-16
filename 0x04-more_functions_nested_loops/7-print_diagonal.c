48e9b891424409098082fa30a6a4acb35978e597
Jude <judo4christ@gmail.com>
2022-12-16 08:07:27 +0000

    commit print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,21 +9,18 @@ void print_diagonal(int n)
 {
 int i, j;
 
-if (n <= 0)
-{
-_putchar('\n');
-return;
-}
-
 for (i = 0; i < n; i++)
 {
-
-for (j = 0; j < (n - 1); j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
-
 _putchar('\\');
+_putchar('\n');
+}
+
+if (n <= 0)
+{
 _putchar('\n');
 }
 }
