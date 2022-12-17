ecf38fb33aa7e65935aefb9ea23bee942030cf0d
Jude <judo4christ@gmail.com>
2022-12-17 03:27:43 +0000

    Commenting out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints :( and \\o/
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -14,7 +14,7 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 /**
  *while (i < 10)
- *          {
+ *{
  *putchar(i);
  *}
  */
