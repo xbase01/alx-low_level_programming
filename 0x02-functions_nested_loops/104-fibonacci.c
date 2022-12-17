2a7ea4a0ae4eef14f6b5020a53b9804d2e3afed8
Jude <judo4christ@gmail.com>
2022-12-17 04:40:56 +0000

    Prints first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
-/**
- *main - prints the first 98 Fibonacci numbers
- *
- *Return: returns 0
- */
+#define LARGEST 10000000000
 
+/**
+ * main - prints fibonnaci numbers
+ *
+ * Description: This function prints the fibonnaci
+ * from 1 to 98
+ *
+ * Return: 0 Success
+ */
 int main(void)
 {
-int first = 1, second = 2, next, c;
+unsigned long int frnt1 = 0, bck1 = 1, frnt2 = 0, bck2 = 2;
+unsigned long int hold1, hold2, hold3;
+int cunt;
 
-printf("%d, %d", first, second);
-
-for (c = 0; c < 96; c++)
+printf("%lu, %lu, ", bck1, bck2);
+for (cunt = 2; cunt < 98; cunt++)
 {
-next = first + second;
-printf(", %d", next);
-first = second;
-second = next;
+if (bck1 + bck2 > LARGEST || frnt2 > 0 || frnt1 > 0)
+{
+hold1 = (bck1 + bck2) / LARGEST;
+hold2 = (bck1 + bck2) % LARGEST;
+hold3 = frnt1 + frnt2 + hold1;
+frnt1 = frnt2, frnt2 = hold3;
+bck1 = bck2, bck2 = hold2;
+printf("%lu%010lu", frnt2, bck2);
 }
-
+else
+{
+hold2 = bck1 + bck2;
+bck1 = bck2, bck2 = hold2;
+printf("%lu", bck2);
+}
+if (cunt != 97)
+printf(", ");
+}
 printf("\n");
-
 return (0);
 }
