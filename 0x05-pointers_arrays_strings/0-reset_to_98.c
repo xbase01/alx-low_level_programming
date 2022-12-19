963e965f169723b7f0abad6cb17bc83dded4e567
Jude <judo4christ@gmail.com>
2022-12-19 19:23:30 +0000

    Pointer first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and
+ *updates the value it points to to 98
+ *@n: the character to check
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+if (n != NULL)
+{
+*n = 98;
+}
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05. C - Pointers, arrays and strings
+
+This directory contains ALx task on pointers, arrays and strings
