40d7f589ba930ab7f75fcd4a5ffbd55117693fd6
Jude <judo4christ@gmail.com>
2022-12-23 04:08:22 +0000

    commit leet and rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,45 +1,27 @@
 #include "main.h"
-/**
- *rot13 - encodes a string using rot13
- *@s: character to check
- *Return: returns 0
- */
 
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
 char *rot13(char *s)
 {
-int i;
+int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] >= 'a' && s[i] <= 'z')
+for (j = 0; a[j] != '\0'; j++)
 {
-
-if (s[i] < 'n')
+if (s[i] == a[j])
 {
-s[i] = s[i] + 13;
-}
-else
-{
-s[i] = s[i] - 13;
+s[i] = b[j];
+break;
 }
 }
 }
-
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] >= 'A' && s[i] <= 'Z')
-{
-
-if (s[i] < 'N')
-{
-s[i] = s[i] + 13;
-}
-else
-{
-s[i] = s[i] - 13;
-}
-}
-}
-
 return (s);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,61 +1,24 @@
 #include "main.h"
 /**
- *leet - encodes a string into 1337
- *@s: character to check
- *Return: returns 0
+ * leet - encodes a string into 1337
+ * @c: char to encode
+ * Return: @c char
  */
-char *leet(char *s)
+char *leet(char *c)
 {
 int i;
+int j;
+char letters[] = "aeotl";
+char up_letters[] = "AEOTL";
+char numbers[] = "43071";
 
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; c[i] != '\0'; i++)
 {
-
-if (s[i] == 'a')
+for (j = 0; letters[j] != '\0'; j++)
 {
-s[i] = '4';
-}
-else if (s[i] == 'e')
-{
-s[i] = '3';
-}
-else if (s[i] == 'o')
-{
-s[i] = '0';
-}
-else if (s[i] == 't')
-{
-s[i] = '7';
-}
-else if (s[i] == 'l')
-{
-s[i] = '1';
+if (c[i] == letters[j] || c[i] == up_letters[j])
+c[i] = numbers[j];
 }
 }
-
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == 'A')
-{
-s[i] = '4';
-}
-else if (s[i] == 'E')
-{
-s[i] = '3';
-}
-else if (s[i] == 'O')
-{
-s[i] = '0';
-}
-else if (s[i] == 'T')
-{
-s[i] = '7';
-}
-else if (s[i] == 'L')
-{
-s[i] = '1';
-}
-}
-
-return s;
+return (c);
 }
