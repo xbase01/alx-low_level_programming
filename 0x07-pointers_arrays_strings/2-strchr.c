84eec691711d7ac3ed139fb85a18158db8be4b4a
Jude <judo4christ@gmail.com>
2023-01-03 17:16:52 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@ char *_strchr(char *s, char c)
 {
 int i;
 
-for (i = 0; s[i] != '\0'; i++)
+i = 0;
+for (;; ++i)
 {
-if (s[i] == c)
-{
-return (&s[i]);
+if (*(s + i) == c)
+return (s + i);
+if (*(s + i) == '\0')
+return (NULL);
 }
-}
-return ('\0');
 }
