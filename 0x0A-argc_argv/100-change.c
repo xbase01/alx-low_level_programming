ef5cb89bff4acac9c09328f3988ef770ad19b551
Jude <judo4christ@gmail.com>
2023-01-08 09:40:32 +0000

    coin change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,59 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: return 0 if successful, 1 if invalid arguments
+ */
 
-   main - entry point
-
-   @argc: argument counter
-
-   @argv: argument vector
-
-   Return: return 0 if successful, 1 if invalid arguments
-*/
 int main(int argc, char *argv[])
 {
-  int cents, coins = 0;
+int cents;
+int coins[] = {25, 10, 5, 2, 1};
+int num_coins = 0;
+int i;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
 
-  cents = atoi(argv[1]);
-  if (cents < 0)
-    {
-      printf("0\n");
-      return (0);
-    }
 
-  while (cents >= 25)
-    {
-      cents -= 25;
-      coins++;
-    }
-  while (cents >= 10)
-    {
-      cents -= 10;
-      coins++;
-    }
-  while (cents >= 5)
-    {
-      cents -= 5;
-      coins++;
-    }
-  while (cents >= 2)
-    {
-      cents -= 2;
-      coins++;
-    }
-  while (cents >= 1)
-    {
-      cents -= 1;
-      coins++;
-    }
-
-  printf("%d\n", coins);
-  return (0);
+for (i = 0; i < 5; i++)
+{
+while (cents >= coins[i])
+{
+cents -= coins[i];
+num_coins++;
+}
+}
+printf("%d\n", num_coins);
+return (0);
 }
