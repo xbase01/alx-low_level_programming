6a166594c563f483370d3082796e57ac31f9f296
Jude <judo4christ@gmail.com>
2023-01-15 19:03:07 +0000

    splits string

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int count_words(char *str);
 
 #endif /* MAIN.H */
