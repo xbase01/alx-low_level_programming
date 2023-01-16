aac9f44b6fd7578ca9554fe0ddf7d61cfa03a14a
Jude <judo4christ@gmail.com>
2023-01-16 05:53:46 +0000

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: input integer
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
