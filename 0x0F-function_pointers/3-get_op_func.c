2dea034b6c4a143f9efe222138e35bbce13320ff
Jude <judo4christ@gmail.com>
2023-01-19 14:10:20 +0000

    last 2 corrections reverted

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,74 @@
-#include "3-calc.h"
-/**
- * get_op_func - selects the correct function to perform the operation
- * @s: operator passed as argument to the program
- * Return: pointer to the function that
- * corresponds to the operator given as a parameter
+/*
+ * functions that adds, subtracts, multiplies, divides and computes remainders,
+ * respectively.
  */
-int (*get_op_func(char *s))(int, int)
-{
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i;
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
-i = 0;
-while (i < 5)
+/**
+ * op_add - adds two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of @a and @b.
+ */
+int op_add(int a, int b)
 {
-if (*(ops[i]).op == *s)
+return (a + b);
+}
 
+/**
+ * op_sub - finds the difference between two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the difference between @a and @b.
+ */
+int op_sub(int a, int b)
 {
-return (ops[i].f);
+return (a - b);
 }
-i++;
+
+/**
+ * op_mul - computes the product of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the product of @a and @b.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
 }
-return (NULL);
+
+/**
+ * op_div - divides an integer by another integer
+ * @a: the dividend (the integer to be divided)
+ * @b: the divisor
+ * Return: the quotient of @a and @b.
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+return (a / b);
+}
+
+/**
+ * op_mod - computes the remainder from the division of @a by @b
+ * @a: the dividend (the integer that is divided)
+ * @b: the divisor (the integer that divides
+ * Return: the remainder from @a divided by @b.
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+return (a % b);
 }
